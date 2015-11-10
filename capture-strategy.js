ac2fc60a9bdc7079cbe2b91367d105b0b3976e94
Sam Breed <sam@quickleft.com>
2015-11-09 21:24:05 -0800

    initial commit 💥

---
+++ capture-strategy.js
@@ -0,0 +1,26 @@
+var casper = require('casper').create({
+  clientScripts: [
+    'node_modules/es5-shim/es5-shim.js'
+  ],
+  viewportSize: {
+    width: 960,
+    height: 800
+  },
+  verbose: true,
+  logLevel: "debug"
+})
+
+casper.start('http://obliquestrategies.website', function enableUnsplash() {
+  this.click('input[name="unsplash"]')
+})
+
+casper.thenEvaluate(function setBackground() {
+  document.querySelector('.controls').remove()
+})
+
+casper.wait(5000, function captureScreenshot() {
+  var filename = this.getHTML('#strategy').toLowerCase().replace(/\s/g, '-')
+  this.capture('assets/' + filename + '.png')
+})
+
+casper.run()
